12e5d9493f7c6f5e121b828c9f133c67e7f82d98
anagy66 <a.nagy66@yahoo.ca>
2026-03-27 16:32:14 +0000

    setup.sql was updated by adding SQL statements for creating new databases and schemas.

--- setup/dbt_demo_setup.sql
+++ setup/dbt_demo_setup.sql
@@ -5,3 +5,9 @@ ALTER SCHEMA analytics.dev SET METRIC_LEVEL = 'ALL';
 ALTER SCHEMA analytics.prod SET LOG_LEVEL = 'INFO';
 ALTER SCHEMA analytics.prod SET TRACE_LEVEL = 'ALWAYS';
 ALTER SCHEMA analytics.prod SET METRIC_LEVEL = 'ALL';
+
+CREATE DATABASE analytics_dev;
+CREATE SCHEMA analytics_dev.dbt_demo;
+
+CREATE DATABASE analytics_prod;
+CREATE SCHEMA analytics_prod.dbt_demo;
